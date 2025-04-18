6e46f6e2724852e53a5ebacf6df56e8c67645212
dlworua <leegaegyum@gmail.com>
2025-04-18 16:32:23 +0900

    style: 주석 제거

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -17,6 +17,7 @@ class HomePage extends StatelessWidget {
             controller: textEditingController,
             decoration: InputDecoration(
               hintText: '검색어를 입력해 주세요',
+              // TextField 상태에 따라 다른 테두리 적용하고 싶을 때!
               border: WidgetStateInputBorder.resolveWith((states) {
                 print(states);
                 if (states.contains(WidgetState.focused)) {
@@ -38,40 +39,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
-
-/*
-    // MaterialStateOutlineInputBorder.resolveWith : 
-    // TextField 상태에 따라 다른 테두리 적용하고 싶을 때!
-      border: MaterialStateOutlineInputBorder.resolveWith(
-      (states) {
-        return OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.grey),
-        );
-      },
-    ),
-  ),
-  // 모바일에서 키보드 유형 설정
-  keyboardType: TextInputType.text,
-  // 키보드의 작업 버튼을 '완료'로 설정
-  textInputAction: TextInputAction.done,
-  // 텍스트 변경 시 호출되는 함수
-  onChanged: (text) {
-    print('Text changed: $text');
-  },
-  // 키보드에서 완료(엔터) 눌렀을 때 호출되는 함수
-  onSubmitted: (text) {
-    print('Submitted text: $text');
-  },
-  // true로 설정하면 입력한 텍스트가 가려짐 (비밀번호 입력 시 사용)
-  obscureText: false, 
-  // 최대 글자 수 제한
-  maxLength: 50,
-  // 최대 입력 줄 수 제한 
-  maxLines: 1, 
-  // TextField 글자 스타일
-  style: TextStyle(fontSize: 16, color: Colors.black), // 입력 텍스트 스타일
-)
-*/
