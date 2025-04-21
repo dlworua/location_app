bbca99ee26227816921839a1a29d6a8cd4649abb
dlworua <leegaegyum@gmail.com>
2025-04-21 14:48:42 +0900

    feat: 디테일페이지 웹뷰 사용하여 네이버 웹 불러오기

--- lib/ui/pages/detail/detail_page.dart
+++ lib/ui/pages/detail/detail_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Text('DetailPage'));
+    return Scaffold(
+      appBar: AppBar(),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: WebUri('https://www.naver.com')),
+      ),
+    );
   }
 }
