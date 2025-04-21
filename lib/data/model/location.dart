e70d0b619d427d45389067a56eb49f366ac7461b
dlworua <leegaegyum@gmail.com>
2025-04-21 15:45:03 +0900

    feat: location파일 만들어서 속성 지정

---
+++ lib/data/model/location.dart
@@ -0,0 +1,35 @@
+/* "title": "삼성1동 주민센터",
+      "link": "http://www.gangnam.go.kr/center/main.do?office=3220047",
+      "category": "공공,사회기관>행정복지센터",
+      "description": "",
+      "telephone": "",
+      "address": "서울특별시 강남구 삼성1동 161-2",
+      "roadAddress": "서울특별시 강남구 봉은사로 616 삼성1동 주민센터",
+      "mapx": "1270625320",
+      "mapy": "375144424" */
+
+import 'dart:io';
+
+class Location {
+  String title;
+  String link;
+  String category;
+  String description;
+  String telephone;
+  String address;
+  String roadAddress;
+  String mapx;
+  String mapy;
+
+  Location({
+    required this.title,
+    required this.link,
+    required this.category,
+    required this.description,
+    required this.telephone,
+    required this.address,
+    required this.roadAddress,
+    required this.mapx,
+    required this.mapy,
+  });
+}
