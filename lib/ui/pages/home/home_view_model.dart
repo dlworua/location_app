2e0d27bf1d864649725ea72c4ad708416200f971
dlworua <leegaegyum@gmail.com>
2025-04-21 18:59:26 +0900

    feat: homeViewModel 상태클래스와 뷰모델, 뷰모델 관리자 만들기

---
+++ lib/ui/pages/home/home_view_model.dart
@@ -0,0 +1,28 @@
+// 1. 상태 클래스 만들기
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location_app/data/model/location.dart';
+import 'package:location_app/data/repository/location_repository.dart';
+
+class HomeState {
+  List<Location> locations;
+  HomeState(this.locations);
+}
+
+// 2. 뷰모델 만들기 - Notifier 상속
+class HomeViewModel extends Notifier<HomeState> {
+  @override
+  HomeState build() {
+    return HomeState([]);
+  }
+
+  void searchLocations(String query) async {
+    final locationRepository = LocationRepository();
+    final locations = await locationRepository.searchLocations(query);
+    state = HomeState(locations);
+  }
+}
+
+// 3. 뷰모델 관리자 만들기 - NotifierProvider 객체
+final homeviewmodelProvider = NotifierProvider<HomeViewModel, HomeState>(() {
+  return HomeViewModel();
+});
