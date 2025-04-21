13ccfb8ab55257a6e1ae630efd8fb335c05dc6c3
dlworua <leegaegyum@gmail.com>
2025-04-21 16:09:30 +0900

    feat: detailPage 인앱 웹뷰 설정

--- lib/ui/pages/detail/detail_page.dart
+++ lib/ui/pages/detail/detail_page.dart
@@ -9,6 +9,10 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(),
       body: InAppWebView(
         initialUrlRequest: URLRequest(url: WebUri('https://www.naver.com')),
+        initialSettings: InAppWebViewSettings(
+          userAgent:
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        ),
       ),
     );
   }
